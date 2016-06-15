3e3aebd7f3a1948e9b18bf5f8764aa2bde76a8e3
Jose Miguel <miguel.gamazo@galiclick.com>
2016-06-15 08:28:06 +0200

    actulizacion a 15 de junio 2016-2

--- delete_arg.js
+++ delete_arg.js
@@ -1,7 +1,7 @@
 var sqlite3 = require('sqlite3').verbose(),
 db = new sqlite3.Database('sewage');
 
-var query = "DELETE * FROM lecturas WHERE hora BETWEEN '" + process.argv[2] + "' AND '" + process.argv[3] + "';";
+var query = "DELETE FROM lecturas WHERE hora BETWEEN '" + process.argv[2] + "' AND '" + process.argv[3] + "';";
 
 db.serialize(function() {
   db.run(query, function(err, rows)
