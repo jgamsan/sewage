6e3e7fe79ecd081106d4b7eefef61bb9a52f980f
Jose Miguel <miguel.gamazo@galiclick.com>
2016-06-15 08:22:33 +0200

    actulizacion a 15 de junio 2016

---
+++ delete_arg.js
@@ -0,0 +1,16 @@
+var sqlite3 = require('sqlite3').verbose(),
+db = new sqlite3.Database('sewage');
+
+var query = "DELETE * FROM lecturas WHERE hora BETWEEN '" + process.argv[2] + "' AND '" + process.argv[3] + "';";
+
+db.serialize(function() {
+  db.all(query, function(err, rows)
+    {
+    if(err) {
+      throw err;
+    }
+    else {
+      console.log("Borrado perfectamente");
+    }
+  });
+});
