96a49a100f27ed365160e5b6635c3f2f41e77239
Jose Miguel <miguel.gamazo@galiclick.com>
2016-06-22 17:19:46 +0200

    actulizacion a 22 de junio 2016-1

--- prueba_1.js
+++ prueba_1.js
@@ -4,22 +4,10 @@ var moment = require('moment');
 
 a = moment().startOf('day').subtract(1, 'days').add(19, 'hours').format();
 b = moment().startOf('day').subtract(1, 'days').add(19, 'hours').add(20, 'minutes').format();
-
-//query = "SELECT hora, altura FROM lecturas where datetime(hora) between " + "datetime(" + String(a) + ")" + " AND datetime(" + String(b) + ");";
-
 query = 'SELECT hora, altura FROM lecturas;';
-//query = 'SELECT hora, altura FROM lecturas;';
-
 db.serialize(function() {
   db.each(query, function(err, row) {
     console.log(row.hora + ": " + row.altura);
   });
 });
 
-var fs = require('fs');
-var writeStream = fs.createWriteStream("file.xls");
-
-
-writeStream.write("Sl No"+"\t"+" Age"+"\t"+"Name"+"\n"+"0"+"\t"+String(21)+"\t"+"Rob"+"\n"+"1"+"\t"+" 22"+"\t"+"bob"+"\n");
-
-writeStream.close();
